2b4e06f9206b1999b5a658f09faa5aa11623d565
ucostanaur-zubiri <34026296+ucostanaur-zubiri@users.noreply.github.com>
2022-12-13 10:44:12 +0100

    Added @EnableWebSecurity to config

--- src/main/java/eus/hekuntza/zubiri/config/SecurityAuthManagerConfig.java
+++ src/main/java/eus/hekuntza/zubiri/config/SecurityAuthManagerConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import eus.hekuntza.zubiri.authentication.providers.OtpAuthenticationProvider;
 import eus.hekuntza.zubiri.authentication.providers.UsernamePasswordAuthenticationProvider;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityAuthManagerConfig {
 
   @Autowired
